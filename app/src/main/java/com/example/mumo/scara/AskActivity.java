f0bc58d00a8d62ccffc0f6e122c86495c4209a54
mumo-dev <samumo.dev@gmail.com>
2019-01-19 16:11:55 +0300

    added functionality to submit question to server

--- app/src/main/java/com/example/mumo/scara/AskActivity.java
+++ app/src/main/java/com/example/mumo/scara/AskActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NavUtils;
 import android.support.v4.content.ContextCompat;
@@ -26,8 +28,25 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.mumo.scara.fragments.SelectPicFramgment;
+import com.example.mumo.scara.model.Question;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.util.Date;
+import java.util.UUID;
 
 public class AskActivity extends AppCompatActivity implements SelectPicFramgment.OnSelectedImageListener {
 
@@ -42,6 +61,10 @@ public class AskActivity extends AppCompatActivity implements SelectPicFramgment
     private Spinner mCategorySpinner;
     private Button mSubmitButton;
 
+    private String uploadUrl;
+
+    private FirebaseFirestore mFirestore = FirebaseFirestore.getInstance();
+    private CollectionReference questionCollectionRef = mFirestore.collection("questions");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,14 +99,117 @@ public class AskActivity extends AppCompatActivity implements SelectPicFramgment
             }
         });
 
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String question = editTextQuestion.getText().toString().trim();
+                String category = mCategorySpinner.getSelectedItem().toString();
+                if (question.length() <= 0) {
+                    Toast.makeText(AskActivity.this, "Question cannot be empty", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (category.equals("Choose")) {
+                    Toast.makeText(AskActivity.this, "Please choose a category", Toast.LENGTH_LONG).show();
+                    return;
+                }
 
+                String imageUrl = "";
+                //check if any image was uploaded..
+                if (mPhotoImageView.getDrawable() != null) {
+                    //upload image to server
+                    String imageRef = UUID.randomUUID().toString();
+                    uploadImageToServer(imageRef);
+                    imageUrl = imageRef;
+                    /* StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("images.jpg");
+                        ImageView image = (ImageView)findViewById(R.id.imageView);
+                        Glide.with(this).using(new FirebaseImageLoader()).load(storageReference).into(image );
+                    */
+                }
+                //create a Question Object
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                String username = user != null ? user.getDisplayName() : "Anonymous";
+                Question q = new Question(question, username, category, 0, imageUrl, new Date());
+                uploadDataToFirebase(q);
+            }
+        });
+
+
+    }
+
+    private void uploadDataToFirebase(Question q) {
+        mSubmitButton.setText("Sending...");
+        mSubmitButton.setEnabled(false);
+        questionCollectionRef.add(q)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        resetUI();
+                        Toast.makeText(AskActivity.this, "Question Uploaded successfully", Toast.LENGTH_LONG).show();
+//                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                resetUI();
+                e.printStackTrace();
+                Toast.makeText(AskActivity.this, "uploadTask failed " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void resetUI() {
+        mSubmitButton.setText("ASK");
+        mSubmitButton.setEnabled(true);
+        editTextQuestion.setText("");
+    }
+
+    private void uploadImageToServer(String name) {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageReference = storage.getReference();
+        final StorageReference ref = storageReference.child("images/" + name);
+
+        mSubmitButton.setText("Sending...");
+        mSubmitButton.setEnabled(false);
+//        final String urlImage;
+        Bitmap bitmap = ((BitmapDrawable) mPhotoImageView.getDrawable()).getBitmap();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+        UploadTask uploadTask = ref.putBytes(data);
+        Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                if (!task.isSuccessful()) {
+                    throw task.getException();
+                }
+                return ref.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                resetUI();
+                mPhotoImageView.setImageDrawable(null);
+                mPhotoImageView.setVisibility(View.GONE);
+                mPhotoRemove.setVisibility(View.GONE);
+                
+                if (task.isSuccessful()) {
+                    Uri downloadUri = task.getResult();
+                    if (downloadUri != null) {
+                        uploadUrl = downloadUri.toString();
+//                        Toast.makeText(AskActivity.this, uploadUrl, Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(AskActivity.this, "Image upload Failed", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     private void checkPermissions() {
         if (!hasStorageWritePermission() && !hasCameraAccessPermission()) {
             //request both permissions
             requestPermission(new String[]{
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA }, ALL_PERMISSIONS);
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, ALL_PERMISSIONS);
         } else if (!hasCameraAccessPermission()) {
             requestPermission(new String[]{Manifest.permission.CAMERA}, PERMISSION_ACCESS_CAMERA);
         } else if (!hasStorageWritePermission()) {
@@ -122,20 +248,20 @@ public class AskActivity extends AppCompatActivity implements SelectPicFramgment
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == ALL_PERMISSIONS){
-            if(grantResults.length > 0 ){
-                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == ALL_PERMISSIONS) {
+            if (grantResults.length > 0) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     //write permission granted
                     //check camera permission
-                    if(grantResults.length > 1){
-                        if(grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                    if (grantResults.length > 1) {
+                        if (grantResults[1] == PackageManager.PERMISSION_GRANTED) {
 
-                        }else {
+                        } else {
                             //camerA permission denied
                             finish();
                         }
                     }
-                }else {
+                } else {
                     //write permission denied, exit
                     finish();
                 }
--- app/src/main/java/com/example/mumo/scara/model/Question.java
+++ app/src/main/java/com/example/mumo/scara/model/Question.java
@@ -1,4 +1,83 @@
 package com.example.mumo.scara.model;
 
+import com.google.firebase.firestore.Exclude;
+
+import java.util.Date;
+
 public class Question {
+    private String questionId;
+    private String text;
+    private String username;
+    private String category;
+    private int votes;
+    private String imageReference;
+    private Date createdAt;
+
+    public Question() {}
+
+    public Question(String text, String username, String category, int votes, String url, Date createdAt) {
+        this.text = text;
+        this.username = username;
+        this.category = category;
+        this.votes = votes;
+        this.imageReference = url;
+        this.createdAt = createdAt;
+    }
+
+    @Exclude
+    public String getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(String questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+    public void setVotes(int votes) {
+        this.votes = votes;
+    }
+
+    public String getImageReference() {
+        return imageReference;
+    }
+
+    public void setImageReference(String imageReference) {
+        this.imageReference = imageReference;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
 }
